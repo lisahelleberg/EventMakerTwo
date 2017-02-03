ff37aeca8444e853b54ea86edc78f7165829bde0
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-02-03 14:20:04 +0100

    Oprettet Event.cs og EventCatalogSingleton.cs

---
+++ EventMakerTwo/Model/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMakerTwo.Model
+{
+    class Event
+    {
+        public int Id;
+        public string Name;
+        public string Description;
+        public string Place;
+        public DateTime DateTime;
+
+        public Event()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
